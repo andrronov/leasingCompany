a194880583f7435d7881e3e0f6800a23d94ce5e9
andrronov <andrew270504cska@gmail.com>
2024-01-23 11:42:10 +0300

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [],
+  darkMode: 'class',
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    screens: {
+      o: "1px",
+       xs: "614px",
+       sm: "1002px",
+       md: "1022px",
+       lg: "1092px",
+       xl: "1280px"
+    }
   },
   plugins: [],
 }
