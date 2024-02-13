3ad3baf4ea395a914bf48d5fb091f6cfdff655a5
Андрей Андронов <andrew270504cska@gmail.com>
2024-02-14 02:35:32 +0300

    static

--- server/index.js
+++ server/index.js
@@ -12,6 +12,7 @@ app.use(bodyParser.json())
 
 app.use('/api', router)
 
+app.use(express.static(process.cwd()+"/dist"))
 app.listen(process.env["PORT"] || PORT, () => {
   console.log(`Сервер запущен на порту ${PORT}`);
 });
