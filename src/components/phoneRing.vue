1ae3657064c16280ca5b66e0d4a3b39aa714ae0f
Андрей Андронов <andrew270504cska@gmail.com>
2024-02-14 02:38:02 +0300

    phone

--- src/components/phoneRing.vue
+++ src/components/phoneRing.vue
@@ -17,4 +17,32 @@
    transform: translate3d(0, 0, 0) scale(1);
  }
  
+ .Phone::before,
+ .Phone::after {
+   position: absolute;
+   content: "";
+ }
+ 
+ .Phone::before {
+   top: 0;
+   left: 0;
+   width: 75px;
+   height: 75px;
+   background-color: rgba(#fff, 0.1);
+   border-radius: 100%;
+   opacity: 1;
+   transform: translate3d(0, 0, 0) scale(0);
+ }
+ 
+ .Phone::after {
+   top: 13.5%;
+   left: 13.5%;
+   width: 55px;
+   height: 55px;
+   background: url(data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNCAyNCI+PHBhdGggZD0iTTYuNiAxMC44YzEuNCAyLjggMy44IDUuMSA2LjYgNi42bDIuMi0yLjJjLjMtLjMuNy0uNCAxLS4yIDEuMS40IDIuMy42IDMuNi42LjUgMCAxIC40IDEgMVYyMGMwIC41LS41IDEtMSAxLTkuNCAwLTE3LTcuNi0xNy0xNyAwLS42LjQtMSAxLTFoMy41Yy41IDAgMSAuNCAxIDEgMCAxLjIuMiAyLjUuNiAzLjYuMS40IDAgLjctLjIgMWwtMi4zIDIuMnoiIGZpbGw9IiNmZmZmZmYiLz48L3N2Zz4=);
+   background-position: 50% 50%;
+   background-repeat: no-repeat;
+   background-size: cover;
+   transform: translate3d(0, 0, 0);
+ }
 </style>
